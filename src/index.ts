5e26a21ca832cf2af2b8c110c1b92a20bf8234e6
Majid Ali <67096621+Majidalee1@users.noreply.github.com>
2022-04-07 01:42:24 +0500

    initial implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("tsnd working fine");
+import { Agent } from "./agent";
+import { Watcher } from "./Watcher";
+
+// create agents
+
+const Agent1 = new Agent("Majid", "./log.json", 1000);
+const Agent2 = new Agent("Sajid", "./log.json", 6000);
+
+// create watcher
+
+const Watcher1 = new Watcher();
+Watcher1.subscribeToAgent(Agent1);
+Watcher1.subscribeToAgent(Agent2);
+
+// start watcher
+Watcher1.start();
